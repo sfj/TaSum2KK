76cbf0a1c880dccbb8acc158fadd67749a457517
Silas F. Jakobsen <silasfontain@gmail.com>
2012-04-27 15:42:10 +0200

    Code cleanup

--- bin/MasterPage.master.cs
+++ bin/MasterPage.master.cs
@@ -13,6 +13,9 @@ public partial class MasterPage : System.Web.UI.MasterPage
         CreateMenu();
     }
 
+    /// <summary>
+    /// Creates menu items and submenus from the database
+    /// </summary>
     private void CreateMenu()
     {
         menuEntities DB = new menuEntities();
@@ -20,53 +23,62 @@ public partial class MasterPage : System.Web.UI.MasterPage
         var punkter = from p in DB.menus where p.level == 0 select p;
 
         foreach(menu p in punkter) {
-            Literal td = new Literal();
-            td.Text = "<td class=\"menupunkt\" onMouseOver=\"justshow('dd" + p.id + "');\" onMouseOut=\"justhide('dd" + p.id + "');\">";
-            Menu.Controls.Add(td);
+            Literal tag = new Literal() 
+            { 
+                Text = "<td class=\"menupunkt\" onMouseOver=\"justshow('dd" + p.id + "');\" onMouseOut=\"justhide('dd" + p.id + "');\">"
+            };
+            Menu.Controls.Add(tag);
 
-            HyperLink link = new HyperLink();
-            link.CssClass = "menu_link";
-            link.Text = p.text_dk;
-            link.NavigateUrl = p.skabelon.filename;
+            HyperLink link = new HyperLink()
+            {
+                CssClass = "menu_link",
+                Text = p.text_dk,
+                NavigateUrl = p.skabelon.filename
+            };            
             Menu.Controls.Add(link);
 
-            Literal div = new Literal();
-            div.Text = "<div class=\"dd_div\" id=\"dd" + p.id + "\">";
-            Menu.Controls.Add(div);
+            tag = new Literal() 
+            { 
+                Text = "<div class=\"dd_div\" id=\"dd" + p.id + "\">" 
+            };
+            Menu.Controls.Add(tag);
 
-            Table sub = new Table();
-            sub.CssClass = "dropdown";
-            sub.CellPadding = 0;
-            sub.CellSpacing = 0;
+            Table sub = new Table() 
+            { 
+                CssClass = "dropdown",
+                CellPadding = 0,
+                CellSpacing = 0
+            };            
 
             var subpunkter = from s in DB.menus where s.level == 1 && s.parent == p.id select s;
 
             foreach (menu s in subpunkter)
             {
                 TableRow row = new TableRow();
-                TableCell cell = new TableCell();
-                cell.CssClass = "dd_menupunkt";
+                TableCell cell = new TableCell()
+                {
+                    CssClass = "dd_menupunkt"
+                };
 
-                HyperLink sublink = new HyperLink();
-                sublink.CssClass = "dd_menulink";
-
-                sublink.Text = s.text_dk;
-                sublink.NavigateUrl = s.skabelon.filename;
+                HyperLink sublink = new HyperLink() 
+                {
+                    CssClass = "dd_menulink",
+                    Text = s.text_dk,
+                    NavigateUrl = s.skabelon.filename
+                };                
 
                 cell.Controls.Add(sublink);
                 row.Cells.Add(cell);
                 sub.Rows.Add(row);
-                System.Diagnostics.Debug.WriteLine("Row added");
             }
             Menu.Controls.Add(sub);
 
-            Literal div_e = new Literal();
-            div_e.Text = "</div>";
-            Menu.Controls.Add(div_e);
-
-            Literal td_e = new Literal();
-            td_e.Text = "</td>";
-            Menu.Controls.Add(td_e);
+            tag = new Literal() 
+            {
+                Text = "</div></td>"
+            };
+            
+            Menu.Controls.Add(tag);
         }
     }
 }
