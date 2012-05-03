046c2fcaa10163ea0478a88229243bc5a7fed75e
Silas F. Jakobsen <silasfontain@gmail.com>
2012-05-03 08:53:23 +0200

    Added deletion to posts in news

--- Kollegie.Web/Controls/RenderControl.ascx.cs
+++ Kollegie.Web/Controls/RenderControl.ascx.cs
@@ -13,6 +13,8 @@ namespace Kollegie.Web.Controls
         public tekst Text { get; set; }
         public bool canEdit;
 
+        tempdbEntities DB = new tempdbEntities();
+
         protected void Page_Load(object sender, EventArgs e)
         {
             
@@ -23,6 +25,7 @@ namespace Kollegie.Web.Controls
             if (canEdit)
             {
                 EditButton.Visible = true;
+                DeleteButton.Visible = true;
             }
             broedtekst.InnerHtml = Text.text_dk;
             overskrift.InnerHtml = Text.id.ToString();
@@ -30,6 +33,14 @@ namespace Kollegie.Web.Controls
 
         protected void EditButtonClick(object sender, EventArgs e)
         {
+            Response.Redirect("Forside.aspx?edit=" + Text.id);
+        }
+
+        protected void DeleteButtonClick(object sender, EventArgs e)
+        {
+            var post = (from p in DB.teksts where p.id == Text.id select p).SingleOrDefault();
+            DB.DeleteObject(post);
+            DB.SaveChanges();
             Response.Redirect("Forside.aspx?edit=" + Text.id);
         }
     }
--- Kollegie.Web/Controls/RenderControl.ascx.designer.cs
+++ Kollegie.Web/Controls/RenderControl.ascx.designer.cs
@@ -22,6 +22,15 @@ namespace Kollegie.Web.Controls {
         protected global::System.Web.UI.HtmlControls.HtmlGenericControl overskrift;
         
         /// <summary>
+        /// DeleteButton control.
+        /// </summary>
+        /// <remarks>
+        /// Auto-generated field.
+        /// To modify move field declaration from designer file to code-behind file.
+        /// </remarks>
+        protected global::System.Web.UI.WebControls.Button DeleteButton;
+        
+        /// <summary>
         /// EditButton control.
         /// </summary>
         /// <remarks>
