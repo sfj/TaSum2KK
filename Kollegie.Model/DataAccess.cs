2c3eb54fb68753e921cf447827170d0bd8c393e2
Silas F. Jakobsen <silasfontain@gmail.com>
2012-05-03 13:47:10 +0200

    fixed db access

---
+++ Kollegie.Model/DataAccess.cs
@@ -0,0 +1,55 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.IO;
+using Kollegie.Model;
+
+/// <summary>
+/// Summary description for DataAccess
+/// </summary>
+public class DataAccess
+{
+
+    private static DataAccess uniqueInstance;
+    private static Object lockObj = new Object();
+
+    private Entities db;
+
+    public Entities DB
+    {
+        get { return db; }
+    }
+
+	public DataAccess()
+	{
+        string constring;
+
+        string filename = "_dbcon.config";
+
+        string file_path = File.Exists(filename) ? filename : Path.Combine(Environment.GetFolderPath(Environment.SpecialFolder.DesktopDirectory), filename);
+
+        TextReader tr = new StreamReader(file_path);
+
+        constring = tr.ReadLine();
+
+        tr.Close();
+
+        System.Diagnostics.Debug.WriteLine("Loaded: " + constring + ", From: " + file_path);
+
+        db = new Entities(constring);
+	}
+
+    public static DataAccess getDataAccess()
+    {
+        if (uniqueInstance == null)
+        {
+            lock(lockObj) 
+            {
+                uniqueInstance = uniqueInstance == null ? new DataAccess() : uniqueInstance;
+            }
+            
+        }
+        return uniqueInstance;
+    }
+}
