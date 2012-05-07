80012d35bbd113409cfe0b7681c6f23d17fd33c2
Andreas Kristoffersen <Krissam@gmail.com>
2012-05-07 15:16:17 +0200

    Added hide buttoqs

--- Kollegie.Web/Controls/NewsEditorControl.ascx.cs
+++ Kollegie.Web/Controls/NewsEditorControl.ascx.cs
@@ -7,28 +7,54 @@ using System.Web.UI.WebControls;
 using Kollegie.Model;
 using System.Diagnostics;
 
-public partial class NewsEditorControl : UserControl {
-	public nyhed Text { get; set; }
+public partial class NewsEditorControl : UserControl
+{
+    public nyhed Text { get; set; }
     private Entities DB = DataAccess.getDataAccess().DB;
 
-	protected override void OnLoad(EventArgs e) {
-		
-	}
+    protected override void OnLoad(EventArgs e)
+    {
 
-	protected override void OnPreRender(EventArgs e) {
+    }
+
+    protected override void OnPreRender(EventArgs e)
+    {
         if (Text != null)
         {
             NewsEditorHead.Value = Text.headline_dk;
             NewsEditor.Value = Text.text_dk;
+            HideButton.Text = (Text.hidden != true) ? "Skjul" : "Vis";
         }
         else
         {
             NewsEditorHead.Value = "";
             NewsEditor.Value = "";
+            HideButton.Visible = false;
         }
-	}
+    }
 
-	protected void SubmitButton_Click(object sender, EventArgs e) {
+    protected void SubmitButtonClick(object sender, EventArgs e)
+    {
+        nyhed ny = GetNyhed();
+        ny.text_dk = NewsEditor.Value;
+        ny.headline_dk = NewsEditorHead.Value;
+        DB.SaveChanges();
+        Text = ny;
+        Response.Redirect(Request.RawUrl);
+    }
+    protected void HideButtonClick(object sender, EventArgs e)
+    {
+        nyhed ny = GetNyhed();
+        if (ny.hidden != null)
+            ny.hidden = !ny.hidden;
+        else
+        {
+            ny.hidden = true;
+        }
+        DB.SaveChanges();
+    }
+    private nyhed GetNyhed()
+    {
         nyhed po;
         if (Text != null)
         {
@@ -40,10 +66,6 @@ public partial class NewsEditorControl : UserControl {
             po.created = new DateTime();
             DB.nyheds.AddObject(po);
         }
-        po.text_dk = NewsEditor.Value;
-        po.headline_dk = NewsEditorHead.Value;
-        DB.SaveChanges();
-        Text = po;
-        Response.Redirect(Request.RawUrl);
-	}
+        return po;
+    }
 }
